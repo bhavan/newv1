560477a05054e884243f3b1e3be54d0c53502084
Yogi Ghorecha <yogi.ghorecha@gmail.com>
2012-07-26 16:27:01 +0530

    photo upload validation

--- inc/base.php
+++ inc/base.php
@@ -543,72 +543,95 @@ else
 
 function m_upload_photo() {
   global $var;
+ 		 
+		
   if(isset($var->post['formname']) && $var->post['formname'] == 'upload.event.photo') {
     //fprint($_FILES); _x();
-    if(($image = file_upload(array(
-      'name'      => 'image',
-      'path'      => $var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS,
-      'savename'  => $_FILES['image']['name']
-    ))) != false) {
-      //fprint($image);
-      //echo $var->abs_srv_path.'images'.DS.'phocagallery'.DS.'thumbs'.DS;
-      image_convert(array(
-        'path'  => $var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS,
-        'name'  => $image,
-        'size'  => array(
-          'phoca_thumb_l_' => '600px',
-          'phoca_thumb_m_' => '180px',
-          'phoca_thumb_s_' => '60px',
-        ),
-      ));
-      $metadesc = array(
-        'username' => $var->post['username'],
-        'location' => $var->post['location']
-      );
-      $sql = "insert into `jos_phocagallery` set 
-          `catid` = ".id_from_phoca_cat($var->photo_upload_cat).", 
-          `title` = '"._clean($var->post['caption'])."', 
-          `alias` = '"._clean(str_replace(' ', '-', strtolower(trim($var->post['caption']))))."', 
-          `filename` = '".$image."', 
-          `approved` = 0, 
-          `description` = '"._clean($var->post['description'])."', 
-          `metadesc` = '".serialize($metadesc)."'";
-      //fprint($sql);
-      db_insert($sql);
-      @copy($var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS.$image, $var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.$image);
-      @unlink($var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS.$image);
-      $var->photo_uploaded = true;
-	 
-    }
-
-    //#DD#
-    if($var->photo_uploaded==true)
-    {
-			$rec = mysql_query("SELECT * FROM `jos_users` WHERE `id`=62");
-			$pageglobal=mysql_fetch_array($rec);
-			$adminEmail = $pageglobal['email'];
-            //$adminEmail	= $adminuser->email;
-			$sitename =  $pageglobal['site_name'];
-			$subject= 'New Photo Uploaded ';
-			$message = '
-			<table cellpadding="3" cellspacing="3">
-			<tr><td colspan="2" align="left"><h2>Photo Details</h2></td></tr>
-			<tr><td align="left"><b>Photo</b> </td><td>: '. "http://{$_SERVER['HTTP_HOST']}/partner/{$_SESSION["partner_folder_name"]}/images/phocagallery/$image".'</td></tr>
-			<tr><td align="left"><b>Photo Caption</b> </td><td>: '.$var->post['caption'].'</td></tr>
-			<tr><td align="left"><b>Photo Description</b> </td><td>: '.$var->post['description'].'</td></tr>
-			<tr><td align="left"><b>Name</b> </td><td>: '.$var->post['username'].'</td></tr>
-			<tr><td align="left"><b>Hometown</b> </td><td>: '.$var->post['location'].'</td></tr>
-			</table>';
-			$headers = 'MIME-Version: 1.0' . "\r\n";
-			$headers .= 'Content-type:text/html;charset=iso-8859-1' . "\r\n";
+	
+		$filename = $_FILES['image']['name'];
+		
+		$ext = substr($filename,(strpos($filename,'.') + 1));
 			
-			$_SERVER['SERVER_NAME'] = str_replace('www.', '', $_SERVER['SERVER_NAME']);
+		if(empty($filename) || $filename == ""){
 			
-			$headers .= 'From: NO-REPLY <admin@'.$_SERVER['SERVER_NAME'].'>' . "\r\n";
-			$headers .= 'X-Mailer: PHP/' . phpversion() . "\r\n";
-			// Email Notification to Administrator
-			mail($adminEmail,$subject,$message,$headers);
-    }
+			$msg="Choose image for upload!";
+			
+		}
+		
+		else if($ext == "gif" || $ext == "GIF" || $ext == "JPEG" || $ext == "jpeg" || $ext == "jpg" || $ext == "JPG" || $ext == "PNG" || $ext == "png")
+		{
+			if(($image = file_upload(array(
+		      'name'      => 'image',
+		      'path'      => $var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS,
+		      'savename'  => $_FILES['image']['name']
+		    ))) != false) {
+		      //fprint($image);
+		      //echo $var->abs_srv_path.'images'.DS.'phocagallery'.DS.'thumbs'.DS;
+		      image_convert(array(
+		        'path'  => $var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS,
+		        'name'  => $image,
+		        'size'  => array(
+		          'phoca_thumb_l_' => '600px',
+		          'phoca_thumb_m_' => '180px',
+		          'phoca_thumb_s_' => '60px',
+		        ),
+		      ));
+		      $metadesc = array(
+		        'username' => $var->post['username'],
+		        'location' => $var->post['location']
+		      );
+		      $sql = "insert into `jos_phocagallery` set 
+		          `catid` = ".id_from_phoca_cat($var->photo_upload_cat).", 
+		          `title` = '"._clean($var->post['caption'])."', 
+		          `alias` = '"._clean(str_replace(' ', '-', strtolower(trim($var->post['caption']))))."', 
+		          `filename` = '".$image."', 
+		          `approved` = 0, 
+		          `description` = '"._clean($var->post['description'])."', 
+		          `metadesc` = '".serialize($metadesc)."'";
+		      //fprint($sql);
+		      db_insert($sql);
+		      @copy($var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS.$image, $var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.$image);
+		      @unlink($var->joomla_root.'partner/'.$_SESSION["partner_folder_name"].'/images'.DS.'phocagallery'.DS.'thumbs'.DS.$image);
+		      $var->photo_uploaded = true;
+			 
+		    }
+		
+		    //#DD#
+		    if($var->photo_uploaded==true)
+		    {
+					$rec = mysql_query("SELECT * FROM `jos_users` WHERE `id`=62");
+					$pageglobal=mysql_fetch_array($rec);
+					$adminEmail = $pageglobal['email'];
+		            //$adminEmail	= $adminuser->email;
+					$sitename =  $pageglobal['site_name'];
+					$subject= 'New Photo Uploaded ';
+					$message = '
+					<table cellpadding="3" cellspacing="3">
+					<tr><td colspan="2" align="left"><h2>Photo Details</h2></td></tr>
+					<tr><td align="left"><b>Photo</b> </td><td>: '. "http://{$_SERVER['HTTP_HOST']}/partner/{$_SESSION["partner_folder_name"]}/images/phocagallery/$image".'</td></tr>
+					<tr><td align="left"><b>Photo Caption</b> </td><td>: '.$var->post['caption'].'</td></tr>
+					<tr><td align="left"><b>Photo Description</b> </td><td>: '.$var->post['description'].'</td></tr>
+					<tr><td align="left"><b>Name</b> </td><td>: '.$var->post['username'].'</td></tr>
+					<tr><td align="left"><b>Hometown</b> </td><td>: '.$var->post['location'].'</td></tr>
+					</table>';
+					$headers = 'MIME-Version: 1.0' . "\r\n";
+					$headers .= 'Content-type:text/html;charset=iso-8859-1' . "\r\n";
+					
+					$_SERVER['SERVER_NAME'] = str_replace('www.', '', $_SERVER['SERVER_NAME']);
+					
+					$headers .= 'From: NO-REPLY <admin@'.$_SERVER['SERVER_NAME'].'>' . "\r\n";
+					$headers .= 'X-Mailer: PHP/' . phpversion() . "\r\n";
+					// Email Notification to Administrator
+					mail($adminEmail,$subject,$message,$headers);
+		    }
+		} 
+		else
+		{
+			$msg="File Type must be GIF or JPG or PNG images only!";
+			$filename="";
+			//return false;
+		}
+    
     //#DD#
 
 
