1c056a5c8e42fc566b68c5303e4dceab19aa884f
Yogi Ghorecha <yogi.ghorecha@gmail.com>
2012-07-26 19:25:14 +0530

    error log and thanks message

--- thanks.php
+++ thanks.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Come Back Soon!</title>
-<style type="text/css">
-<!--
-.towncenter {
- text-align: center; margin-top: 50px;
-}
-.towncenter table tr td {
- font-family: Verdana, Geneva, sans-serif; line-height: 30px;
-}
--->
-</style>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Come Back Soon!</title>
+	<style type="text/css">
+		.towncenter {
+		 text-align: center; margin-top: 50px;
+		}
+		.towncenter table tr td {
+		 font-family: Verdana, Geneva, sans-serif; line-height: 30px;
+		}
+	</style>
 </head>
 
 <body class="towncenter">
@@ -22,7 +20,7 @@
   </tr>
 
   <tr>
-    <td><p>Hello and thanks for visiting! The TownWizard site that you are looking for is currently unavailable. If you are a current partner, please feel free to contact TownWizard Support here(link to http://townwizard.zendesk.com). If you are not a current TownWizard partner and would like to be your own TownWizard, please visit http://www.townwizard.com! <a style="color:#EE3F43;" href="http://townwizard.zendesk.com" target="_blank">here</a> if you have any additional questions.</p>
+    <td><p>Hello and thanks for visiting! The TownWizard site that you are looking for is currently unavailable. If you are a current partner, please feel free to contact TownWizard Support <a style="color:#EE3F43;" href="http://townwizard.zendesk.com" target="_blank">here</a>. If you are not a current TownWizard partner and would like to be your own TownWizard, please visit <a href="http://www.townwizard.com" target="_blank">http://www.townwizard.com</a></p>
     <p>Thank You!</p></td>
   </tr>
 
