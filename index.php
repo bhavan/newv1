f5ab9e2e32e3455dd69ad0e4c961b085a4fd4ec7
yryogi <yogi.ghorecha@gmail.com>
2012-11-27 22:42:26 +0530

    json and Safari detection

--- index.php
+++ index.php
@@ -70,8 +70,36 @@ $data['_date'] = $temp[0];
 <!-- use favicon icon for v2 -->
 <link rel="shortcut icon" href="partner/<?php echo $_SESSION['partner_folder_name'];?>/images/favicon.ico" />
 
-<!--  Town wizard Google Analytic code -->
+<!--  CODE for SAFARI BROWSER DETECTION BEGIN -->
+	<script>
+	// First Time Visit Processing
+	// copyright 10th January 2006, Stephen Chapman
+	// permission to use this Javascript on your web page is granted
+	// provided that all of the below code in this script (including this
+	// comment) is used without any alteration
+	function rC(nam) {var tC = document.cookie.split('; '); for (var i = tC.length - 1; i >= 0; i--) {var x = tC[i].split('='); if (nam == x[0]) return unescape(x[1]);} return '~';} function wC(nam,val) {document.cookie = nam + '=' + escape(val);} function lC(nam,pg) {var val = rC(nam); if (val.indexOf('~'+pg+'~') != -1) return false; val += pg + '~'; wC(nam,val); return true;} function firstTime(cN) {return lC('pWrD4jBo',cN);} function thisPage() {var page = location.href.substring(location.href.lastIndexOf('\/')+1); pos = page.indexOf('.');if (pos > -1) {page = page.substr(0,pos);} return page;}
 
+	// example code to call it - you may modify this as required
+	function start() {
+	   if (firstTime(thisPage())) {
+	      // this code only runs for first visit
+	     if((navigator.userAgent.match(/iphone/i)) || (navigator.userAgent.match(/ipad/i)) || (navigator.userAgent.match(/ipod/i))) {
+	  var r=confirm("Install iphone app on your device: Click ok to Install");
+	   if (r==true){window.location = "<?php echo $var->iphone?>";}
+	     }
+	     else if (navigator.userAgent.match(/android/i)) {
+	     var r=confirm("Install android app on your device: Click ok to Install");
+	     if (r==true){location.href="<?php echo $var->android?>";}
+	 }else {}
+	   }
+	   // other code to run every time once page is loaded goes here
+	}
+	onload = start;
+
+	</script>
+<!--  CODE for SAFARI BROWSER DETECTION END -->
+
+<!--  Town wizard Google Analytic code -->
 <?php include("ga.php"); ?>
 </head>
 
