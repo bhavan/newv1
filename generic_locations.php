49879e71ca56a8adc19b9e53245195e33e7e2f3d
yryogi <yogi.ghorecha@gmail.com>
2013-01-03 21:43:11 +0530

    Generic location page

---
+++ generic_locations.php
@@ -0,0 +1,140 @@
+<?php
+global $var;
+
+/**
+Purpose: Code for Joomla menu
+Developer: Yogi
+Last Updated Date: 27-12-2012
+**/
+//require('jmenu.php');
+/** Code for Joomla menu End **/
+
+include_once('./inc/var.php');
+include_once($var->inc_path.'base.php');
+_init();
+
+//#DD#
+$searchdata = '';
+if($_POST['search_rcd']=="Search Places" || $_POST['search_rcd']=="Lugares de la búsqueda") {
+	$searchdata = trim($_POST['searchvalue']);
+}
+
+/* 
+Code Begin 
+Result  : Listing of locations from CATEGORY ID
+Request : Fetching category id from URL 
+Developer:Rinkal 
+Last update Date:02-01-2013
+*/
+
+$category_id = $_REQUEST['category_id'];
+
+$res=mysql_query("select title from jos_categories where id=".$category_id."");
+	while($loc_title=mysql_fetch_array($res))
+	{
+	$loc_cat_name=$loc_title['title'];
+	$loc_cat_name=explode(" -", $loc_cat_name);
+		
+	}
+	
+/* code End */
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title><?php echo $var->site_name.' | '.$loc_cat_name[0]; ?></title>
+<link rel="image_src" href="http://<?php echo $_SERVER['HTTP_HOST']?>/partner/<?php echo $_SESSION['partner_folder_name']?>/images/logo/logo.png" />  
+<meta property="og:image" content="http://<?php echo $_SERVER['HTTP_HOST']?>/partner/<?php echo $_SESSION['partner_folder_name']?>/images/logo/logo.png"/>
+<meta property="og:title" content="<?php echo $var->site_name.' | '.$var->page_title; ?>"/>
+<meta property="og:description" content="<?php echo strip_tags(m_places_intro()); ?>"/>
+
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<meta name="keywords" content="<?php echo $var->keywords; ?>" />
+<meta name="description" content="<?php echo $var->metadesc; ?>" />
+<meta name="description" content="<?php echo $var->extra_meta; ?>" />
+<script>
+  document.createElement('header');
+  document.createElement('nav');
+  document.createElement('section');
+  document.createElement('article');
+  document.createElement('aside');
+  document.createElement('footer');
+</script>
+<link rel="stylesheet" type="text/css" href="common/templatecolor/<?php echo $_SESSION['style_folder_name'];?>/css/all.css" media="screen" />
+<!--[if IE 7]><link rel="stylesheet" type="text/css" href="common/templatecolor/<?php echo $_SESSION['style_folder_name'];?>/css/ie7.css" media="screen" /><![endif]-->
+<?php include("ga.php"); ?>
+</head>
+
+<body>
+
+<header>
+	<?php m_header(); ?> <!-- header -->
+</header>
+<div id="wrapper">
+	<aside>
+    <?php m_aside(); ?>
+	</aside> <!-- left Column -->
+	<section>
+	
+	
+    <h2><?php echo $var->site_name; ?> <?php echo $loc_cat_name[0]; ?></h2>
+    <?php
+		/* Code added for generic_locations.tpl */
+		require($var->tpl_path."generic_locations.tpl");
+	?>
+    <div class="adThreeHunderd" style="float:left;">
+      <?php m_show_banner('Website Front Page Feature'); ?>
+    </div> <!-- adThreeHunderd -->
+    
+    <!--#DD#--> 
+    <div style="text-align:center;">
+    <form action="" method="post" name="location_form">
+    <input type="text" name="searchvalue" value="<?php echo $searchdata; ?>" size="35" style="margin-top:20px;" />
+    <?php
+	/* Code added for location_button.tpl */
+	require($var->tpl_path."location_button.tpl");
+	?>
+    </form>
+    </div>
+    <!--#DD#--> 
+
+
+    <?php
+	/* 
+		Code Begin 
+		Result  : Listing of locations from CATEGORY ID
+		Request : Fetching category id from URL 
+		Developer:Rinkal 
+		Last update Date:02-01-2013
+	*/
+	
+	
+	$rec=mysql_query("select * from jos_categories where (parent_id=".$category_id." OR id=".$category_id.") AND section='com_jevlocations2' and published=1 order by `ordering`") or die(mysql_error());
+	
+	/*Code End */
+	
+	while($row=mysql_fetch_array($rec))
+	{
+		if (m_location_count($row['title'],false, $searchdata))
+		{
+	?>
+    <div style="float:left;width:720px;display:block;">
+    <h2><?=$row['title'];?></h2>
+      
+        <?php m_location_list($row['title'],false, $searchdata); ?>
+     
+     <br clear="all"> 
+     </div>
+    <?php
+	}}
+	?>
+	</section> <!-- rightColumn -->
+</div> <!-- wrapper -->
+<footer>
+	<?php m_footer(); ?> <!-- footer -->
+</footer>
+
+</body>
+</html>
